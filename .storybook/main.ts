18523429698715b8b61d9d0a373b1e95ac52142d
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-08-23 01:41:13 +0300

    chore: cleanup storybook base components, adjust config to accept path aliases

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import * as path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,18 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  async viteFinal(config, options) {
+    if (config?.resolve?.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@/': path.resolve(__dirname, '../src'),
+        '@/components': path.resolve(__dirname, '../src/components'),
+        '@/hooks': path.resolve(__dirname, '../src/hooks'),
+        '@/utils': path.resolve(__dirname, '../src/utils'),
+      };
+    }
+    return config;
   },
 };
 export default config;
