f081d0561ca86d9ff945d7701b711249e086931a
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-08-23 23:07:38 +0300

    chore: remove storybook default assests, add calendar story

---
+++ src/stories/Calendar.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CalendarComponent from '@/components/Calendar';
+
+const meta: Meta<typeof CalendarComponent> = {
+  title: 'Calendar/Calendar',
+  component: CalendarComponent,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Calendar: Story = {
+  args: {},
+};
