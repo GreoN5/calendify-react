c7688a811c864180e2dd2cdcbefd2bcdcf6f5c51
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2023-09-02 19:14:41 +0300

    feat: add modal support, remove custom date actions, add conditional props

--- src/utils/props.ts
+++ src/utils/props.ts
@@ -1,9 +1,9 @@
-import { CheckboxProps, CustomActionsProps } from '@/components/Calendar/Calendar';
-
-export const isOfTypeCustomActionsProps = (props: unknown): props is CustomActionsProps => {
-  return Boolean((props as CustomActionsProps)?.dateActions);
-};
+import { CheckboxProps, ModalProps } from '@/components/Calendar/Calendar';
 
 export const isOfTypeCheckboxProps = (props: unknown): props is CheckboxProps => {
   return Boolean((props as CheckboxProps)?.useCheckbox);
+};
+
+export const isOfTypeModalProps = (props: unknown): props is ModalProps => {
+  return Boolean((props as ModalProps)?.useModal);
 };
